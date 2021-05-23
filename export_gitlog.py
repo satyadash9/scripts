49561d8b6d125df939a7b7b77b07e5e3cd7dc726
Satyanarayana Dash <satya.d135@gmail.com>
2021-05-23 09:08:29 +0530

    Updated script # 21thMay chk and del dir func added

--- export_gitlog.py
+++ export_gitlog.py
@@ -1,18 +1,34 @@
 import subprocess
 import os
+import shutil
 
 clone_cmd = "git clone git@github.com:satyanarayanadash/project1"
-export_gitlog = "git log > gitlog.txt | ls -la"
+export_gitlog = "git log > gitlog.txt | ls"
+
+repo_name = clone_cmd.split("/")[1]
+repo_path = os.getcwd() + "/" + repo_name
+
+def chk_del_dir(path):
+    if os.path.exists(path) is True:
+        shutil.rmtree(path)
+        print("\n", "Deleted existing repo directory", "\n")
+        subp_run(clone_cmd)
+    else:
+        print("\n", "Repo directory does not exist", "\n")
+        subp_run(clone_cmd)
 
 def subp_run(cmd):
-    subprocess.run(cmd, shell=True)
+        subprocess.run(cmd, shell=True)
 
-def change_dir():
-    repo_name = clone_cmd.split("/")[1]
-    repo_path = os.getcwd() + "/" + repo_name
-    os.chdir(repo_path)
-    print("Current working directory ->", os.getcwd())
+def change_dir(path):
+    try:
+        os.chdir(path)
+        print("Changed current working directory", "\n")
+        subp_run(export_gitlog)
+    except FileNotFoundError as e:
+        print(e)
+    except Exception as e:
+        print(e)
 
-subp_run(clone_cmd)
-change_dir()
-subp_run(export_gitlog)
+chk_del_dir(repo_path)
+change_dir(repo_path)
